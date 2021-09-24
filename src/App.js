7f8230ab145d08bf6c35dd6bf722d967b05ff870
facundorsabia <facundorsabia@gmail.com>
2021-09-24 17:01:40 -0300

    Instalacion Bootstrap y desarrollo Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.css'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Este es mi primer proyecto de React <code>src/App.js</code> 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
     </div>
   );
 }
