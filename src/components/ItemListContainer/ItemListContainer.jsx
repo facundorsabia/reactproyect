4962e9034a7b4c99e1205c500461f7e64601142f
Facundo Rodriguez Sabia <facundorsabia@gmail.com>
2021-11-01 20:25:42 -0300

    firebase

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -8,13 +8,17 @@ const ItemListContainer = () => {
 const[items, setItems] = useState([]);
 const {category } = useParams()
 
-useEffect(() => {
-        const db = getFirestore();
-        db.collection('cursos').get() //toda la coleccion
-        .then (resp => setItems(resp.docs.map(it =>({id:it.id,...it.data()}) )))
- //       db.collection('cursos').doc('eEqv8rkdlGfu6JPN7Nhu').get() //toda la coleccion
- //       .then (resp => console.log(resp))
-}, [category]);
+useEffect(() =>{
+    const db = getFirestore();
+    // decidir que coleccion quiero traer de firebase
+    db.collection('Items').get()//toda la coleccion
+    .then(resp => setItems(resp.docs.map(it => ({id: it.id, ...it.data() }))))//capturar la promesa
+
+    // db.collection('Items').doc('TxUPSN56lp6OuPyOIPYN').get()//traer uno
+    // .then(resp => setItem({id: resp.id, ...resp.data() } ) )
+  }, [category])
+
+  console.log(items);
 
 console.log(items)
 
